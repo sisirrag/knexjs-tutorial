0bed9e84789399522e095478cc01888440698d5b
sisirrag <mca549@rajagiri.edu>
2022-04-08 23:33:39 +0530

    objection

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const dbSetup = require('./db/db-setup');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./routes');
+const Book =require('./db/models/book');
+const { user } = require('pg/lib/defaults');
+dbSetup();
 const app=express();
 const port = 8080;
+
+
+app.get('/book/:id',async (req,res,next)=>{
+    try {
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+        
+    }
+    const{id} = req .params;
+    const book = await Book.query().findById(id);
+    res.json(book);
+});
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
