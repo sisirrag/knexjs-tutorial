285f45b0532743edce967033358009213be45edb
sisirrag <mca549@rajagiri.edu>
2022-04-10 21:23:08 +0530

    delete

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const cors = require('cors');
 const router = require('./routes');
 const Book =require('./db/models/book');
 const { user } = require('pg/lib/defaults');
-dbSetup();
 const app=express();
 const port = 8080;
+
+dbSetup();
+
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(router);
+app.use(cors());
+
 
 
 app.get('/book/:id',async (req,res,next)=>{
@@ -18,15 +27,56 @@ app.get('/book/:id',async (req,res,next)=>{
         res.status(500).json(err);
         
     }
-    const{id} = req .params;
+    const{id} = req.params;
     const book = await Book.query().findById(id);
     res.json(book);
 });
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(router);
+app.delete('/book/:id',async (req,res,next)=>{
+    try {
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+        
+    }
+    const{id} = req.params;
+    const book = await Book.query().deleteById(id);
+    res.send('Book is deleted');
+});
+
+app.post('/book/:id',async (req,res,next)=>{
+    try {
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+        
+    }
+    const{id} = req.params;
+    const book = await Book.query().findById(id).patch({
+        isbn: 88,
+        title: 'edited'
+      });
+    res.send('Book is edited');
+});
+
+app.get('/books', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+        
+    }
+    const books = await Book.query();
+    console.log(books.length);
+    res.json(books);
+});
+
+
+
+
 
 
 app.listen(port,()=>console.log('server listening'));
